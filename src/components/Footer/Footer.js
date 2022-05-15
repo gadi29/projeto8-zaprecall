4f95ed4eaac97cebec1dd1241b64a83188d2b8cf
gadiel <gad.azevedo@gmail.com>
2022-05-15 18:31:48 -0300

    modifications in footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,23 @@
 import "./style.css";
+import festa from "../../assets/images/party.png"
 
 export default function Footer(props) {
     
     let questions = [...props.questions];
     
+    if (props.answered === questions.length) {
+        return (
+            <div className="footer finished">
+                <div className="principal">
+                    <img src={festa} alt="Emoji Festejando" />
+                    <h1>Parabéns!</h1>
+                </div>
+                <h3>Você não esqueceu de nenhum flashcard!</h3>
+                <h2>{props.answered}/{questions.length} CONCLUÍDOS</h2>
+            </div>
+        );
+    }
+
     return (
         <div className="footer">
             <h2>{props.answered}/{questions.length} CONCLUÍDOS</h2>

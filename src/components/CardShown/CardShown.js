56d26e7a10c7eb9a1eb02243be8e1660f8b793f6
gadiel <gad.azevedo@gmail.com>
2022-05-15 15:43:48 -0300

    turn answer

--- src/components/CardShown/CardShown.js
+++ src/components/CardShown/CardShown.js
@@ -1,15 +1,38 @@
+import React from "react"
 import "./style.css";
 import setinha from "../../assets/images/setinha.png"
 
+function Button(props) {
+    return(
+        <>
+            <button className={props.class}>
+                {props.text}
+            </button>
+        </>
+    );
+}
+
 export default function CardShown(props) {
+    const [quest, setQuest] = React.useState(true);
     let cardQuest=props.question;
 
     return (
         <>
+            { quest ?
             <div className="card question">
                 {cardQuest.question}
-                <img src={setinha} alt="Setinha"/>
+                <img onClick={() => setQuest(false)} src={setinha} alt="Setinha"/>
             </div>
+            :
+            <div className="card question answer">
+                {cardQuest.answer}
+                <div className="buttons">
+                    <Button text="Não lembrei" class="red" />
+                    <Button text="Quase não lembrei" class="orange" />
+                    <Button text="Zap!" class="green" />
+                </div>
+            </div>
+            }
         </>
     )
 }
